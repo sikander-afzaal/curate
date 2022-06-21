3e131a415d29d6c927123dde8508c1642cab347b
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-21 15:23:41 +0500

    commit

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Contact.css";
 function Contact() {
-  return <div className="contact">Contact</div>;
+  return (
+    <div className="contact">
+      <h1 className="title roboto">Contact Us</h1>
+      <p className="roboto">
+        Fill up the form below & our team will get back soon.
+      </p>
+      <form action="#">
+        <div className="input-row">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <textarea placeholder="Your Message"></textarea>
+        <p className="note roboto">
+          By submitting this form you agree to our terms and conditions and our
+          Privacy policy.
+        </p>
+        <button className="submit-form">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
